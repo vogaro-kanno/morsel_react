a15b2b8e7d20bca4e8bae595ed6b9b43b58b80bd
vogaro-kanno <k.kanno@vogaro.co.jp>
2023-08-25 18:37:31 +0900

    [add]ベース作成

---
+++ src/app/about/info/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Info</div>;
+};
+
+export default Page;
---
+++ src/app/about/layout.tsx
@@ -0,0 +1,9 @@
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode,
+}) {
+  return (
+    <div className="flex justify-center items-center h-screen">{children}</div>
+  );
+}
